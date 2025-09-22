1f8ffc897fd512372755358c5c2ef3f25cbc53e2
Daniel Borsheim <daniel01.kodehode@gmail.com>
2025-09-22 14:38:26 +0200

    styles: fix links, responsive CSS

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ const hamburger = document.querySelector(".hamburger");
 const hamburgerTop = document.querySelector(".line.top");
 const hamburgerMiddle = document.querySelector(".line.middle");
 const hamburgerbottom = document.querySelector(".line.bottom");
+const mobileLinks = document.querySelectorAll(".nav-overlay a");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
   mobileOverlay.classList.remove("preload");
   mobileOverlay.classList.toggle("open");
+  body.style.overflow = body.style.overflow === "hidden" ? "auto" : "hidden";
+});
+
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileOverlay.classList.remove("open");
+  });
 });
